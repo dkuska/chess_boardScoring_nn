ea139f106aa5f305e5a5c8873c065b9984d9e722
David Simon Kuska <dkuska@uni-potsdam.de>
2022-05-12 17:31:06 +0200

    Added randomness into ai move selection & fixed automatic termination

--- main.py
+++ main.py
@@ -3,25 +3,23 @@ import utils
 
 import chess
 import torch
-import time
+import numpy as np
 
 play_mode = "player_vs_ai" if False else "ai_vs_ai"
 
 net_type = "NN" if True else "CNN"
+model_path = "chess_fc_singledropout_500k.pth" if net_type == "NN" else ""
+
+# This parameter is used to insert some randomness into the game
+# TODO: Tune this!
+epsilon = 0.5
 
 
 def game_init():
     board = chess.Board()
 
-    if net_type == "NN":
-        state_dict = torch.load("chess_500k.pth", map_location=torch.device('cpu'))
-        nn = net.Net()
-    elif net_type == "CNN":
-        state_dict = torch.load("chess_cnn_500k.pth", map_location=torch.device('cpu'))
-        nn = net.Net()
-    else:
-        return
-
+    state_dict = torch.load(model_path, map_location=torch.device('cpu'))
+    nn = net.Net()
     nn.load_state_dict(state_dict, strict=False)
 
     return board, nn
@@ -30,8 +28,8 @@ def game_init():
 # Color == Color of CPU
 # Net returns positive values for favourable board for white and negative values for favourable board for black
 def determine_next_move(color: str, board, nn):
-    best_score = -100000 if color == "w" else 100000
-    best_move = chess.Move.null()
+    move_list = []
+    score_list = []
 
     for move in board.legal_moves:
         if move == chess.Move.null():
@@ -42,66 +40,71 @@ def determine_next_move(color: str, board, nn):
         fen = board.fen()
         fen_vector = utils.fen_to_bit_vector(fen)
         fen_tensor = torch.from_numpy(fen_vector).byte()
+        with torch.no_grad():
+            score = nn(fen_tensor)[0].item()
 
-        score = nn(fen_tensor)[0]
+        score_list.append(score)
+        move_list.append(move)
 
-        if color == "w":
-            if score > best_score:
-                # print("{} is better than {}".format(score, best_score))
-                best_score = score
-                best_move = move
-        elif color == "b":
-            if score < best_score:
-                best_score = score
-                best_move = move
-
-        # print("Move: {}, Score: {}".format(move, score))
         board.pop()
+
+    if color == "w":
+        best_score = max(score_list)
+    else:
+        best_score = min(score_list)
+
+    # Good moves are all those, whose score is within range of the best score
+    good_moves = [move_list[i] for i in range(len(move_list)) if abs(best_score - score_list[i]) <= epsilon]
+    best_move = np.random.choice(good_moves)
+
     return best_move
 
 
 def input_move(board):
-    while True:
+    legal_move = False
+    move = chess.Move.null()
+    while not legal_move:
         move_str = input("Your Move: ")
         move = chess.Move.from_uci(move_str)
-        if move in board.legal_moves:
-            return move
-        else:
+        if move not in board.legal_moves or move == chess.Move.null():
             print("Invalid move, try again:")
+        else:
+            return move
+            legal_move = True
+    return move
+
 
 def ai_vs_ai():
     ai_black = net.Net()
     ai_white = net.Net()
 
     ai_white.load_state_dict(torch.load("chess_fc_fulldropout_500k.pth", map_location=torch.device('cpu')))
-    ai_black.load_state_dict(torch.load("chess_fc_singledropout_500k.pth", map_location=torch.device('cpu')))
+    ai_black.load_state_dict(torch.load("chess_fc_fulldropout_500k.pth", map_location=torch.device('cpu')))
 
     board = chess.Board()
 
+    move_counter = 0
+
     while not board.is_checkmate() and not board.is_stalemate() and not board.is_insufficient_material():
+        move_counter += 1
         white_move = determine_next_move("w", board, ai_white)
         board.push(white_move)
-        print("White_AI decided: {} is the best move".format(white_move))
+        print(f"Move {move_counter} - White_AI decided: {white_move}")
         print(board)
 
+        move_counter += 1
         black_move = determine_next_move("b", board, ai_black)
         board.push(black_move)
-        print("Black_AI decided: {} is the best move".format(black_move))
+        print(f"Move {move_counter} - Black_AI decided: {black_move} is the best move")
         print(board)
 
-        print("-----------")
-        # time.sleep(10)
-
-    if board.is_checkmate():
-        reason = "Checkmate"
-    elif board.is_stalemate():
-        reason = "Stalemate"
-    elif board.is_insufficient_material():
-        reason = "Insufficient Material"
-    else:
-        reason = "WTF"
-
-    print("Game ended because: ", reason)
+        outcome = board.outcome()
+        if outcome is not None:
+            print("Game over: ", outcome)
+            print("Winner: ", outcome.winner)
+            print("Reason: ", outcome.termination)
+            print("Result: ", outcome.result())
+            break
 
 
 def game_loop():
@@ -119,9 +122,8 @@ def game_loop():
     while not board.is_checkmate() and not board.is_stalemate() and not board.is_insufficient_material():
         if ai_color == "w":
             ai_move = determine_next_move(ai_color, board, nn)
-            print("AI decided: {} is the best move".format(ai_move))
             board.push(ai_move)
-
+            print(f"### AI decided: {ai_move} ###")
             print(board)
 
             player_move = input_move(board)
@@ -129,24 +131,22 @@ def game_loop():
         else:
             player_move = input_move(board)
             board.push(player_move)
+            print(board)
 
             ai_move = determine_next_move(ai_color, board, nn)
-            print("AI decided: {} is the best move".format(ai_move))
             board.push(ai_move)
+            print(f"### AI decided: {ai_move} ###")
 
         print(board)
         print("---------")
 
-    if board.is_checkmate():
-        reason = "Checkmate"
-    elif board.is_stalemate():
-        reason = "Stalemate"
-    elif board.is_insufficient_material():
-        reason = "Insufficient Material"
-    else:
-        reason = "WTF"
-
-    print("Game ended because: ", reason)
+        outcome = board.outcome()
+        if outcome is not None:
+            print("Game over: ", outcome)
+            print("Winner: ", outcome.winner)
+            print("Reason: ", outcome.termination)
+            print("Result: ", outcome.result())
+            break
 
 
 if __name__ == '__main__':
